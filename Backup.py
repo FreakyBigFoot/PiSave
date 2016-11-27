527a6b3b33faa8557825f84765f6f0dbd586ccac
FreakyBigFoot <mustbaj@gmail.com>
2016-11-27 12:53:04 -0800

    V1.3
    Added the functionality for .extension wildcards to resolve the .state1, .state2 issue.
    No longer prints metadata on files bigger than 4mb. (Chunk Size)

--- Backup.py
+++ Backup.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-import fnmatch, os, dropbox, time
+import re, fnmatch, os, dropbox, time
 from dropbox.files import WriteMode
 
 #This Is Your Unique Key Given By Dropbox
@@ -11,13 +11,14 @@ overwrite = WriteMode('overwrite', None)
 CHUNK_SIZE = 4 * 1024 * 1024
 
 #Add File Types You'd Like To Backup Here
-filetypes = ('.state', '.srm')
+filetypes = '(.state*|.srm*)'
 
 num = 0
 numdone = 0
 for root, dirs, files in os.walk(r'/home/pi/RetroPie/roms'):
     for saves in files:
-        if saves.endswith(filetypes):
+        #if saves.endswith(filetypes):
+        if re.search(filetypes, saves):
             num += 1
         
 print str(num) + ' Saves Found'
@@ -26,7 +27,8 @@ for root, dirs, files in os.walk(r'/home/pi/RetroPie/roms'):
     for saves in files:
         file_path = root + '/' + saves
         dest_path = root + '/' + saves
-        if saves.endswith(filetypes):              
+        #if saves.endswith(filetypes):
+        if re.search(filetypes, saves):              
             f = open(file_path)
             file_size = os.path.getsize(file_path)
             if file_size <= CHUNK_SIZE:
@@ -47,10 +49,14 @@ for root, dirs, files in os.walk(r'/home/pi/RetroPie/roms'):
                         print dbx.files_upload_session_finish(f.read(CHUNK_SIZE),
                                                         cursor,
                                                         commit)
+                        numdone += 1
+                        print str(numdone) + '/' + str(num) + ' saves uploaded'
                     else:
                         dbx.files_upload_session_append(f.read(CHUNK_SIZE),
                                                         cursor.session_id,
                                                         cursor.offset)
                         cursor.offset = f.tell()
+                        numdone += 1
+                        print str(numdone) + '/' + str(num) + ' saves uploaded'
             f.close()
             time.sleep(2)
